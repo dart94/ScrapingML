ff32fe716b2f2a06982452a7c4dcfd20853b1b81
Diego Rivera <167586944+dart94@users.noreply.github.com>
2024-06-05 15:25:47 -0700

    modo oscuro

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -49,41 +49,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-document.addEventListener('DOMContentLoaded', function () {
-
-    eventListeners();
-
-    darkMode();
+document.getElementById('theme-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
 });
-
-function darkMode() {
-    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
-
-    // console.log(prefiereDarkMode.matches);
-
-    if (prefiereDarkMode.matches) {
-        document.body.classList.add('dark-mode');
-    } else {
-        document.body.classList.remove('dark-mode');
-    }
-
-    prefiereDarkMode.addEventListener('change', function () {
-        if (prefiereDarkMode.matches) {
-            document.body.classList.add('dark-mode');
-        } else {
-            document.body.classList.remove('dark-mode');
-        }
-
-    });
-    const botonDarkMode = document.querySelector('.dark-mode-boton');
-    botonDarkMode.addEventListener('click', function () {
-        document.body.classList.toggle('dark-mode');
-
-        // Guardar el estado del modo oscuro en localStorage
-        if (document.body.classList.contains('dark-mode')) {
-            localStorage.setItem('darkMode', 'enabled');
-        } else {
-            localStorage.setItem('darkMode', 'disabled');
-        }
-    });
-}
